a4767ef4c62c3e9a966abeb1aae11d114793976b
ya-its-yasin <yaitsyasin@gmail.com>
2024-02-21 12:40:02 +0000

    end of day18

--- day12-rest-api/src/main/java/com/tcs/rest/resources/ProductResource.java
+++ day12-rest-api/src/main/java/com/tcs/rest/resources/ProductResource.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tcs.rest.resource.Product;
 import com.tcs.rest.services.ProductService;
 
+@CrossOrigin(origins = "http://localhost:4200") //port number of angular application
 @RestController
 @RequestMapping("/product/v1")
 public class ProductResource {
@@ -37,7 +39,7 @@ public class ProductResource {
 		return service.getProducts();
 	}
 
-	@GetMapping("product/{id}")
+	@GetMapping("/product/{id}")
 	public Optional<Product> fetchProduct(@PathVariable int id) {
 		return service.getProduct(id);
 	}
